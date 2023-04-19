8af9e3eb000794f2545910edc6313167fd187301
Thomas Koller <thomaskoller90@icloud.com>
2023-04-19 21:29:09 +0200

    feat: added first implementation of FastMQTT.subscribe
    With the subscribe decorator one can subscribe topics

--- src/fastmqtt/application.py
+++ src/fastmqtt/application.py
@@ -1,2 +1,35 @@
+import typing
+
+import paho.mqtt.client as mqtt
+
+from .signature import CallbackSignature
+from .topic import Topic
+
+
 class FastMQTT:
-    pass
+    def __init__(self) -> None:
+        self._broker = mqtt.Client()
+        self._broker.connect(host="localhost", port=1883)
+        self._broker.on_message = self._on_message
+        self._topics: typing.Dict[Topic, CallbackSignature] = {}
+
+    def subscribe(self, topic: str) -> typing.Callable:
+        def inner(func: typing.Callable):
+            self._topics[topic] = CallbackSignature.load(callback=func)
+            self._broker.subscribe(topic=topic)
+
+            def wrapped(msg):
+                func(msg=msg)
+
+            return wrapped
+
+        return inner
+
+    def run(self) -> typing.NoReturn:
+        self._broker.loop_forever()
+
+    def _on_message(self, client, user, msg) -> None:
+        self._get_callback_signature(topic=msg.topic)(payload=msg.payload)
+
+    def _get_callback_signature(self, topic: Topic) -> CallbackSignature:
+        return self._topics[topic]
